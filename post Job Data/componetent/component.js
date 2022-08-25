9251b8206b1e49f8b1f11fb7349e8cf655d42875
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2022-08-25 19:08:01 +0530

    adding profile and signin page

---
+++ post Job Data/componetent/component.js
@@ -0,0 +1,19 @@
+function navbar(){
+    return 
+    `<div>
+    <div id="logodiv">
+      <img
+        id="logo"
+        src="https://www.pngkit.com/png/full/1009-10092379_indeed-logo-white-indeed-logo.png"
+        alt=""
+      />
+      <p id="logotag">for employers</p>
+    </div>
+    <h2 class="menu">Post a job</h2>
+  </div>
+  <div id="lastdiv">
+    <h2 class="menu">Help Center</h2>
+    <button id="signinbutton">Sign in</button>
+    <h2 class="menu">Find jobs</h2>
+  </div>`
+}
