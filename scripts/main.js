e591e0f5cfb414d349d07c5594714533a72a333f
akshay <akshaykadu24@gmail.com>
2022-08-26 14:54:32 +0530

    day4

--- scripts/main.js
+++ scripts/main.js
@@ -16,43 +16,71 @@ let getdata = async()=>{
 
 let append = (data)=>{
     let jobs_container = document.getElementById('jobs_container')
+    jobs_container.innerHTML = null
     data.forEach((elem)=>{
-        let atitle = document.createElement('h1')
+        let atitle = document.createElement('h2')
         atitle.innerText = elem.Title
         let acompany = document.createElement('p')
-        acompany.innerText = elem.compoany
-        // let alocation = document.createElement('p')
-        // alocation.innerText = elem.location
+        acompany.innerText = elem.company
+        let arating = document.createElement('h4')
+        arating.innerText = elem.rating
+        let div2 = document.createElement('div')
+        div2.append(acompany,arating)
+        div2.setAttribute('id','comp_rating')
+
+        let alocation = document.createElement('p')
+        alocation.innerText = elem.city
         let amonthlysalary  = document.createElement('p')
-        amonthlysalary .innerText =  `${elem.monthlysalary}a month `
+        amonthlysalary .innerText =  `${elem.monthlysalary} per month `
         let ajobtype = document.createElement('p')
         ajobtype.innerText = elem.JobType
+        let div1 = document.createElement('div')
+        div1.append(amonthlysalary,ajobtype)
+        div1.setAttribute('id','sal_type')
+        
         let aAboutRole  = document.createElement('p')
         aAboutRole .innerText =  elem.AboutRole
        
         let div = document.createElement('div')
-        div.append(atitle,acompany,amonthlysalary,ajobtype,aAboutRole)
+        div.append(atitle,div2,alocation,div1,aAboutRole)
+        div.setAttribute('id','jobs')
+        div.addEventListener('click',()=>{
+            renderDom(elem)
+        })
+
         jobs_container.append(div)
     })
 
 }
 
 
-let renderDom = ({Title,compoany,JobType, AboutRole,monthlysalary})=>{
+let renderDom = ({Title,company,city,JobType,rating,Description,preferd_skills, AboutRole,monthlysalary})=>{
     let container = document.getElementById('container')
     container.innerHTML = null
-        let atitle = document.createElement('h1')
+        let atitle = document.createElement('h2')
         atitle.innerText = Title
         let acompany = document.createElement('a')
         acompany.href = 'compoany.html'
-        acompany.innerText = compoany
+        acompany.innerText = company
+        let arating = document.createElement('h4')
+        arating.innerText = rating
+        let div2 = document.createElement('div')
+        div2.append(acompany,arating)
+        div2.setAttribute('id','comp_rating')
         
-        // let alocation = document.createElement('p')
-        // alocation.innerText = location
+        let alocation = document.createElement('p')
+        alocation.innerText = city
         let amonthlysalary  = document.createElement('p')
-        amonthlysalary .innerText =  `${monthlysalary} a month `
+        amonthlysalary .innerText =  `${monthlysalary} per month `
         let ajobtype = document.createElement('p')
         ajobtype.innerText = JobType
+        let div1 = document.createElement('div')
+        div1.append(amonthlysalary,ajobtype)
+        div1.setAttribute('id','sal_types')
+        
+        let hr = document.createElement('hr')
+        let role = document.createElement('h2')
+        role.innerText = 'About Role'
         let aAboutRole  = document.createElement('p')
         aAboutRole .innerText =  AboutRole
         let button = document.createElement('button')
@@ -60,10 +88,38 @@ let renderDom = ({Title,compoany,JobType, AboutRole,monthlysalary})=>{
         button.addEventListener('click',()=>{
             buttonchange(button)
         })
+        let job_detail = document.createElement('h2')
+        job_detail.innerText= 'Job detail'
+        let sal = document.createElement('h4')
+        sal.innerText= 'Salary'
+        let salary  = document.createElement('p')
+        salary .innerText =  `${monthlysalary} per month `
+        let typ = document.createElement('h4')
+        typ.innerText= 'Job Type'
+        let type = document.createElement('p')
+        type.innerText = JobType
+        let div3 = document.createElement('div')
+        div3.append(sal,salary,typ,type,hr)
 
+        let skills = document.createElement('h2')
+        skills.innerText = 'Skills'
+        let askills = document.createElement('p')
+        askills.innerText = preferd_skills
+        let Desc = document.createElement('h2')
+        Desc.innerText = 'Description'
+        let aDescription = document.createElement('p')
+        aDescription.innerText = Description
+
+        let hr1 = document.createElement('hr')
+        let hr2 = document.createElement('hr')
+        let hr3 = document.createElement('hr')
+        let div4 = document.createElement('div')
+        div4.append(skills,askills,hr1,Desc,aDescription,hr2)
+
+        
 
         let div = document.createElement('div')
-        div.append(atitle,acompany,amonthlysalary,ajobtype,aAboutRole,button)
+        div.append(atitle,div2,div1,alocation,button,hr,job_detail,div3,hr3,div4,role,aAboutRole)
         container.append(div)
     
 
@@ -72,10 +128,26 @@ let renderDom = ({Title,compoany,JobType, AboutRole,monthlysalary})=>{
 let buttonchange = (button)=>{
     if(button.innerText == 'Apply now'){
         button.innerText = 'Applied';
-        button.setAttribute('Style','bg:green');
+        button.style.backgroundColor = 'green';
     }else{
         button.innerText = 'Apply now'
-        button.setAttribute('Style','Color:blue');
+        button.style.backgroundColor = 'rgb(37,87,167)';
 
     }
+}
+
+let submit = document.getElementById('submit')
+submit.addEventListener('click',()=>{
+    submitting()
+})
+
+let submitting = async()=>{
+    let t_search = document.getElementById('title').value
+    let c_search = document.getElementById('loca').value
+console.log(t_search,c_search)
+    let res = await fetch(`https://pacific-refuge-88537.herokuapp.com/api/Indded?Title=${t_search}&city=${c_search}`)
+    let data = await res.json()
+    append(data)
+    console.log(data)
+    renderDom(data[0])
 }
